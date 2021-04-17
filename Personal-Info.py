e08a4345b7f2862a1ec295d5cb8f881cf7c0dfea
frances917 <82714096+frances917@users.noreply.github.com>
2021-04-17 22:48:48 +0800

    Add files via upload

---
+++ Personal-Info.py
@@ -0,0 +1,10 @@
+
+
+# Personal Info
+
+print("Hi, my name is Frances Bea Villaronte of BIT-23")
+print("I am 20 years old currently living at Imus Cavite")
+print("Born on September 17, 2000")
+print("Daughter of Lenen Villaronte Bantigue and Marlo Gocon")
+print("I am interested in coding and learn about this course")
+print("I love to watch korean drama and play mobile games")
